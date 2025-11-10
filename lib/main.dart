26a778b2db7895edaf5705e4c1a43fdce5dd4405
Laboratory 07 <lab07@lab.mu.edu>
2025-11-10 15:21:00 +0200

    final Submission

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,111 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const HowDoYouFeelToday());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class HowDoYouFeelToday extends StatelessWidget {
+  const HowDoYouFeelToday({super.key});
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: HowDoYouFeelToday(),
     );
+    }
+    }
+
+  
+
+  class HowDoYouFeelTodayApp extends StatefulWidget{
+    const HowDoYouFeelTodayApp({super.key});
+    @override
+  State<HowDoYouFeelTodayApp> createState() => _HowDoYouFeelTodayAppState();
   }
-}
+    
+  
+  class _HowDoYouFeelTodayAppState extends State <HowDoYouFeelTodayApp>{
+    String userChoice='?';
+    String message='How do you feel today';
+    String ComputerChoice='reset';
+      @override
+      Widget build(BuildContext context) {
+    
+    throw UnimplementedError();
+      }
+
+  }
+  final Map<String, String> emojiMap = {
+    'Happy' : ' 😁',
+    'tired': '😒',
+    'Angry': '😡',
+
+  };
+  final List<String> userChoice =[
+    'Happy', 'tired' ,'Angry'
+  ];
+  void _feel (String feeling){
+    final random = Random();
+    final computerMove = userChoice[random.nextInt(userChoice.length);
+    setState(() {
+      userChoice = emojiMap[feeling]!;
+      computerChoice = emojiMap[computerMove]!;
+
+
+  }
+
+  void _reset() {
+    setState(() {
+      userChoice ='0';
+      message='How do you feel today?';
+    });
+  }
+  
+  void setState(Null Function() param0) {
+  }
+
+
+@override 
+Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: const Text('How Do You feel today?'),
+        centerTitle: true,
+      
+      
+      ),
+      body: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    const Text(
+                      '?',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                   ),
+                    
+                  ],
+                ),
+              ],
+            ),
+
+};
+
+  
+
+     
+     
+     
+     
+  
